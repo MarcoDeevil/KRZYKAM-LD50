6f02bcfb7cc7dac2c98efbe2651feb1a91cacc46
Krzysztof <krzysaaa7@gmail.com>
2022-04-03 04:06:55 +0200

    depressing words falling

--- game.js
+++ game.js
@@ -12,6 +12,29 @@ var increaseLineFallSpeedOnFrame = 0.011
 var maxLineFallSpeed = 2
 var minLineFallSpeed = 0.21
 
+//to tez moze byc losowane z jakiegos przedzialu
+//i wtedy wpushowywane tez do obiektu w dropDownWords
+var textFallSpeed = 3;
+
+const sadWords = ["depression", "sorrow"];
+const renderedSadWords = [];
+
+const dropDownWords = () => {
+    const start_x = Math.floor(Math.random()*canvas.width - 100)+101;
+    const fallSpeed = Math.floor(Math.random()*4)+3;
+    console.log(fallSpeed);
+    renderedSadWords.push({
+        word: sadWords[Math.floor(Math.random()*sadWords.length)],
+        x: start_x, 
+        y: 50,
+        fallSpeed: fallSpeed
+    });
+    
+}
+
+// SETINTERVAL /
+
+
 function drawLine(lineCurrentPoint) {
     increaceLineFallSpeed()
     lineDrawPoint = lineDrawPoint + lineFallSpeed;
@@ -38,15 +61,31 @@ function increaceLineFallSpeed() {
 }
 
 setInterval(function () {
+
+
+
     checkLife();
     ctx.clearRect(0, 0, canvas.width, canvas.height); 
     drawLine(lineDrawPoint);
     drawPlayer();
 
+    renderedSadWords.forEach(element => {
+        ctx.font = "30px Arial";
+        if(element.y >= lineDrawPoint){
+            element.y = lineDrawPoint
+        }
+        else {
+            element.y += element.fallSpeed;
+        }
+        ctx.fillText(element.word, element.x, element.y);
+    })
+
 }, 1000 / 60);
 
 //space down
 document.addEventListener("keydown", function(e) {
+    //to nie powinno byc na spacje, na spacje powinny byc dobre a te jakos czasowo? a moze tez na spacje lolz do przemyslenia
+    dropDownWords();
     if(lineFallSpeed > minLineFallSpeed){
         if(e.keyCode === 32){
             if (!fired){
@@ -62,4 +101,3 @@ document.addEventListener("keyup", function(e) {
         fired = false;
     }
 });
-
