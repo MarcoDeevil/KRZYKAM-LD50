19d67a51b6a8574f2c915d7658aa9252df5952da
Krzysztof <krzysaaa7@gmail.com>
2022-04-03 16:55:24 +0200

    merged changes

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ var ctx = canvas.getContext('2d');
 var fired = false;
 
 var playerSize = 50;
+var score = 0;
 
 var decreaseLineFallSpeedOnClick = -0.09
 var increaseLineFallSpeedOnFrame = 0.011
@@ -19,8 +20,14 @@ let isPlayerDead = false;
 //i wtedy wpushowywane tez do obiektu w dropDownWords
 var textFallSpeed = 3;
 
+var sadWordsTimeRender = 20;
+var currentSadWordsTimeRender = sadWordsTimeRender;
+
 const sadWords = ["depression", "sorrow"];
 const renderedSadWords = [];
+const goodWords = ["you can do it", "keep it up", "Love For All, Hatred For None", "Change the world by being yourself", "Every moment is a fresh beginning",
+ "Never regret anything that made you smile", "Die with memories, not dreams"]
+const renderedGoodWords = [];
 
 const dropDownWords = () => {
     const start_x = Math.floor(Math.random() * canvas.width - 100) + 101;
@@ -32,11 +39,22 @@ const dropDownWords = () => {
         y: 50,
         fallSpeed: fallSpeed
     });
+}
 
+const shootGoodWords = () => {
+    const offset_x = Math.floor(Math.random()*-6)+3;
+    const riseSpeed = Math.floor(Math.random()*4)+3;
+    console.log(riseSpeed);
+    renderedGoodWords.push({
+        word: goodWords[Math.floor(Math.random()*goodWords.length)],
+        x: canvas.width/2-25, 
+        y: playerLifeLine,
+        riseSpeed: riseSpeed,
+        offset_x: offset_x
+    });
 }
 
 // SETINTERVAL /
-
 
 function drawLine(lineCurrentPoint) {
     increaceLineFallSpeed()
@@ -56,11 +74,10 @@ function checkLife() {
         // Death of a Player
         if (lineDrawPoint + playerSize >= playerLifeLine) {
             console.log("śmierć, ale jaka?");
-            showEndingScreen(200);
+            showEndingScreen(score);
             isPlayerDead = true;
         }   
     }
-
 }
 
 function increaceLineFallSpeed() {
@@ -69,38 +86,68 @@ function increaceLineFallSpeed() {
     }
 }
 
-setInterval(function () {
+function goodWordsUpdate() {
+    renderedGoodWords.forEach(element => {
+        ctx.font = "30px Arial";
+        ctx.fillStyle = 'rgb(0, 200, 0)';
+        if(element.y <= lineDrawPoint+50){
+            element.y = lineDrawPoint+50
+        }
+        else {
+            element.y -= element.riseSpeed;
+            element.x += element.offset_x;
+        }
+        ctx.fillText(element.word, element.x, element.y);
+    })
+}
 
-
-
-    checkLife();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawLine(lineDrawPoint);
-    drawPlayer();
-
+function sadWordsUpdate() {
     renderedSadWords.forEach(element => {
         ctx.font = "30px Arial";
-        if (element.y >= lineDrawPoint) {
-            element.y = lineDrawPoint
+        ctx.fillStyle = 'rgb(0, 0, 0)';
+
+        if(element.y >= lineDrawPoint){
+         element.y = lineDrawPoint
         }
         else {
             element.y += element.fallSpeed;
         }
         ctx.fillText(element.word, element.x, element.y);
     })
+}
+
+function spawnSadWords() {
+    currentSadWordsTimeRender -= 1;
+    if(currentSadWordsTimeRender == 0){
+        dropDownWords();
+        currentSadWordsTimeRender = 20;
+    }
+}
+ 
+setInterval(function () {
+   
+    checkLife();
+    ctx.clearRect(0, 0, canvas.width, canvas.height); 
+    drawLine(lineDrawPoint);
+    drawPlayer();
+    spawnSadWords();
+    goodWordsUpdate();
+    sadWordsUpdate();
 
 }, 1000 / 60);
 
 //space down
-document.addEventListener("keydown", function (e) {
-    //to nie powinno byc na spacje, na spacje powinny byc dobre a te jakos czasowo? a moze tez na spacje lolz do przemyslenia
-    dropDownWords();
-    if (lineFallSpeed > minLineFallSpeed) {
-        if (e.keyCode === 32) {
-            if (!fired) {
-                lineFallSpeed += decreaseLineFallSpeedOnClick
-                fired = true
-            }
+
+document.addEventListener("keydown", function(e) {
+
+    if(lineFallSpeed > minLineFallSpeed){
+        if(e.keyCode === 32){
+            if (!fired){
+                shootGoodWords();
+                score += 1;
+                lineFallSpeed += decreaseLineFallSpeedOnClick;
+                fired = true;
+            } 
         }
     }
 });
