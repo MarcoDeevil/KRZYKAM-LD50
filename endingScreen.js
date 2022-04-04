8c9b5a314ea2d418b725f6f0b9b1fc35dbeb339e
Krzysztof <krzysaaa7@gmail.com>
2022-04-05 01:53:38 +0200

    final poliszes

--- endingScreen.js
+++ endingScreen.js
@@ -12,9 +12,9 @@ export function showEndingScreen(score) {
         quoteDiv.style = `background:url("https://i.postimg.cc/WpwtR88r/Image-Name-Here-1.png");
         background-size: contain;
       background-repeat: no-repeat;
-      padding-left: 95px;`
+      padding-left:0px;`
         templateBackground.style = "background:#ffda79;"
-        quoteText.style = "margin-top: 85px;margin-left: 50px; color:#12CBC4"
+        quoteText.style = "margin-top: 0px;margin-left: 50px; color:#12CBC4"
         quoteText.textContent = "The more you are motivated by love, the more fearless and free your action will be."
     } else if (score < 30) {
         quoteText.textContent = "It's impossible to find hapiness outside, you have to find it in yourself - trust me it's there"
